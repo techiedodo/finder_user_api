622a3d0a7b9d035120866bc8e678e5fe63bec26b
Luis Gaytan <agaytan@techiedodo.com>
2019-08-27 00:33:32 -0700

    search complete and spinner working

--- src/App.js
+++ src/App.js
@@ -22,12 +22,25 @@ class App extends Component {
 
     this.setState({ users: res.data, loading: false });
   }
+
+  // The function is taking 'text' fomr Search.js and is using it here
+  searchUsers = async text => {
+    const res = await axios.get(
+      `http://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          {/* the prop searchUsers is being passed down to Search.js */}
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 export class Search extends Component {
   state = {
     text: ""
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired
   };
 
   onChange = e => {
@@ -11,7 +16,8 @@ export class Search extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
   };
   render() {
     return (
@@ -30,6 +36,12 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        <button
+          className="btn btn-light btn-block"
+          onClick={this.props.clearUsers}
+        >
+          Clear
+        </button>
       </div>
     );
   }
